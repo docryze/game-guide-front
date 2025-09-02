0ebd2c964b2b0f44bdf84ac5cf64a97f687c8aba
dongkang <docryze@163.com>
2025-09-02 15:19:10 +0800

    switch theme

--- DEV.md
+++ DEV.md
@@ -7,3 +7,13 @@
 | props        | 组件名 + Props           | UserProfileProps |
 | 事件 (emits) | 组件名 + Emits 或 Events | MyButtonEmits    |
 | 插槽 (slots) | 组件名 + Slots           | LayoutSlots      |
+
+#### 2.vue3 defineProps defineModel
+
+| 特性     | defineProps                  | defineModel                   |
+| -------- | ---------------------------- | ----------------------------- |
+| 数据流   | 单向（父 -> 子）             | 双向（父 <-> 子）             |
+| 可修改性 | 不可直接修改                 | 可像 ref 一样直接读写         |
+| 主要用途 | 通用数据传递和展示           | 简化 v-model 的双向绑定       |
+| 底层实现 | 只接收 props                 | 封装了 props + emits          |
+| 最佳实践 | 几乎所有非双向绑定的父子通信 | 所有需要 v-model 的自定义组件 |
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <n-config-provider :theme="themeType">
+  <n-config-provider :theme="activeDarkTheme ? darkTheme : null">
     <div style="height: 100%">
       <n-layout position="absolute">
-        <n-layout-header style="height: 64px; padding: 24px" bordered> Header </n-layout-header>
+        <n-layout-header style="height: 64px; padding: 24px" bordered>
+          <n-flex justify="space-between" wrap="false">
+            <n-flex justify="start">Header</n-flex>
+            <n-flex justify="end">
+              暗色主题
+              <n-switch :value="activeDarkTheme" @update:value="activeThemeChange" />
+            </n-flex>
+          </n-flex>
+        </n-layout-header>
         <n-layout has-sider position="absolute" style="top: 64px; bottom: 64px">
           <n-layout-sider bordered content-style="padding: 24px;">
-            <SideMenu :menuOptions="menuOptions"/>
+            <SideMenu :menuOptions="menuOptions" />
           </n-layout-sider>
           <n-layout content-style="padding: 24px;">
             <router-view></router-view>
@@ -20,12 +28,18 @@
 </template>
 
 <script setup lang="ts">
-import { darkTheme, lightTheme } from 'naive-ui'
+import { ref } from 'vue'
+import { darkTheme } from 'naive-ui'
 import SideMenu from '@/components/menu/SideMenu'
-const themeType = darkTheme;
+const props = defineProps()
+const activeDarkTheme = ref(true)
 const menuOptions = [
   { id: 1, name: 'Home' },
   { id: 2, name: 'About' },
   { id: 3, name: 'Contact' },
 ]
+function activeThemeChange(value: boolean) {
+  activeDarkTheme.value = value
+}
+console.log(props)
 </script>
